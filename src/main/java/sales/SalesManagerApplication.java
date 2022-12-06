78bbe683ce849908993b13bda4fe5b17875b46e1
d1av <davi4alves@gmail.com>
2022-12-06 19:29:04 -0300

    Initial commit from Spring Boot

---
+++ src/main/java/sales/SalesManagerApplication.java
@@ -0,0 +1,13 @@
+package sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagerApplication.class, args);
+	}
+
+}
