babaf6ca9bae3ff5985f6bf11b89b71d59dcae61
d1av <davi4alves@gmail.com>
2022-12-10 20:10:48 -0300

    feat: implemented UserServiceImpl

---
+++ src/main/java/s/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,28 @@
+package s.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UsuarioServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private PasswordEncoder encoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		if(!username.equals("cicrano")) {
+			throw new UsernameNotFoundException("Usuário não encontrado na base.");
+		}
+		
+		return User.builder()
+				.username("cicrano")
+				.password(encoder.encode("123"))
+				.roles("USER", "ADMIN")
+				.build();
+	}
+
+}
